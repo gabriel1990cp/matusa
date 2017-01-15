51e38353c50f7cea230b981d5198f422460ba5e0
Gabriel Costa Pinto <gabriel.cp1990@gmail.com>
2017-01-15 14:09:26 -0200

    Projeto hospedado

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/matusa/';
+
+$dominio= $_SERVER['HTTP_HOST'];
+$url = "http://" . $dominio. $_SERVER['REQUEST_URI'];
+
+$config['base_url'] = $url;
 
 /*
 |--------------------------------------------------------------------------
